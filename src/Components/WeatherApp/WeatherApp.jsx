5f597e96daed40d4b17c8f5792f2a713fc57c7fa
Samanthaw <namicomcor@gmail.com>
2023-09-12 18:54:24 +0700

    Fix leak secret api key

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,15 +11,14 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 const WeatherApp = () => {
-
-  let api_key = "2a292261bca98f4937be38c4be763e5a";
+  let api_key = "your_api_key_here";
+  // Let's go to https://home.openweathermap.org/ to get api key
 
   const [wicon, setWicon] = useState(cloud_icon);
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
-    if(element[0].value==="")
-    {
+    if (element[0].value === "") {
       return 0;
     }
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
@@ -28,47 +27,50 @@ const WeatherApp = () => {
     let data = await response.json();
     const humidity = document.getElementsByClassName("humidity-percent");
     const wind = document.getElementsByClassName("wind-rate");
-    const temperature  = document.getElementsByClassName("weather-temp");
+    const temperature = document.getElementsByClassName("weather-temp");
     const location = document.getElementsByClassName("weather-location");
 
     humidity[0].innerHTML = data.main.humidity + " %";
-    wind[0].innerHTML = data.wind.speed + " km/h";
-    temperature[0].innerHTML = data.main.temp + " °C";
+    wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
+    temperature[0].innerHTML = Math.floor(data.main.temp) + " °C";
     location[0].innerHTML = data.name;
 
-    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") 
-    {
+    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
       setWicon(clear_icon);
-    } 
-    else if (data.weather[0].icon === "02d" || data.weather[0].icon === "02n")
-    {
+    } else if (
+      data.weather[0].icon === "02d" ||
+      data.weather[0].icon === "02n"
+    ) {
       setWicon(cloud_icon);
-    } 
-    else if (data.weather[0].icon === "03d" || data.weather[0].icon === "03n")
-    {
+    } else if (
+      data.weather[0].icon === "03d" ||
+      data.weather[0].icon === "03n"
+    ) {
       setWicon(drizzle_icon);
-    }
-    else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n")
-    {
+    } else if (
+      data.weather[0].icon === "04d" ||
+      data.weather[0].icon === "04n"
+    ) {
       setWicon(drizzle_icon);
-    }
-    else if (data.weather[0].icon === "09d" || data.weather[0].icon === "09n")
-    {
+    } else if (
+      data.weather[0].icon === "09d" ||
+      data.weather[0].icon === "09n"
+    ) {
       setWicon(rain_icon);
-    }
-    else if (data.weather[0].icon === "10d" || data.weather[0].icon === "10n")
-    {
+    } else if (
+      data.weather[0].icon === "10d" ||
+      data.weather[0].icon === "10n"
+    ) {
       setWicon(wind_icon);
-    }
-    else if (data.weather[0].icon === "13d" || data.weather[0].icon === "13n")
-    {
+    } else if (
+      data.weather[0].icon === "13d" ||
+      data.weather[0].icon === "13n"
+    ) {
       setWicon(snow_icon);
-    }
-    else
-    {
+    } else {
       setWicon(clear_icon);
     }
-  }
+  };
 
   return (
     <div className="container">
